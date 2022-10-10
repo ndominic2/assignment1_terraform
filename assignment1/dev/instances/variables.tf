7e281ae69c6d9ab07c2e25265edaa37f83a02607
ndominic2 <103581511+ndominic2@users.noreply.github.com>
2022-10-09 22:11:21 -0400

    Update variables.tf

--- assignment1/dev/instances/variables.tf
+++ assignment1/dev/instances/variables.tf
@@ -12,7 +12,7 @@ variable "instance_type" {
 
 # Variable to signal the current environment 
 variable "env" {
-  default     = "string"
+  default     = "dev"
   type        = string
   description = "Deployment Environment"
 }
